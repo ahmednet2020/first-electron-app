2594a287b8c17b77c841b546496ae4485222f419
ahmednet <ahmednet03@gmail.com>
2018-09-01 22:05:14 +0200

    members

--- src/ts/js/apiKey.ts
+++ src/ts/js/apiKey.ts
@@ -2,10 +2,12 @@ import { ChatManager, TokenProvider } from '@pusher/chatkit';
 
 const instanceLocator: string = 'v1:us1:76612074-e007-4f81-8094-7a6657e66b85';
 const url: string = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/76612074-e007-4f81-8094-7a6657e66b85/token';
-
 let user: any;
-const messagesList: any = document.querySelector("#messagesList");
-const roomsUl: any = document.querySelector("#rooms-ul"); 
+const messagesList: HTMLElement = <HTMLElement>document.querySelector("#messagesList");
+const roomsUl: HTMLElement = <HTMLElement>document.querySelector("#rooms-ul");
+const membersUl: HTMLElement = <HTMLElement>document.querySelector("#members-ul"); 
+
+
 const chatManager:ChatManager = new ChatManager({
 	instanceLocator,
 	userId: 'admin',
@@ -19,14 +21,14 @@ chatManager.connect()
 
 function getRooms(currentUser: any) {
 	roomsUl.innerHTML = '';
-	Promise.all(currentUser.rooms.map((room: any) => {
-		return printRoom(room);
-	})).then((roomArry: any) => {
-		roomArry.map((room: any) => {
+	currentUser.rooms
+		.sort((a: any, b: any) => a.name - b.name)
+		.map((room: any) => {
+			return printRoom(room);
+		}).map((room: any) => {
 			room.firstElementChild.addEventListener('click', Subscriptions);
 			roomsUl.appendChild(room);
 		})
-	}).catch((error: any) => { console.log(error) })
 }
 function printRoom(room: any)
 {
@@ -40,10 +42,11 @@ function printRoom(room: any)
 	return li;
 }
 
-function Subscriptions(e: Event)
+function Subscriptions(e: Event): void
 {
 	e.preventDefault();
 	let roomId = Number(this.dataset.id);
+	let el = this.parentNode;
 	messagesList.innerHTML = '';
 	for (let key in user.roomSubscriptions) {
 		user.roomSubscriptions[key].cancel()
@@ -58,6 +61,15 @@ function Subscriptions(e: Event)
 		},
 		messageLimit: 5
 	})
+	getUsers(roomId);
+	// toggel class to active room
+	Array.prototype.filter.call(el.parentNode.children, function(child: any) {
+		if (child !== el) {
+			child.classList.remove('active');
+		}
+		el.classList.add('active');
+		return child;
+	});
 }
 
 function printMessage(message:any):string {
@@ -69,4 +81,27 @@ function printMessage(message:any):string {
             </div>
           </div>`;
 	return messageString;
+}
+
+function getUsers(roomId:any) {
+	let users: any = user.rooms.filter((room: any) => room.id === roomId);
+	let usersString: any = printUsers(users[0].users);
+	membersUl.innerHTML = usersString;
+
+}
+
+function printUsers(users:any[]) {
+	let members: any[] = users.map((member:any) => {
+		let state: string = 'offline';
+		if (member.presence.state === 'online')
+		{
+			state = 'online';
+		}
+		let memberString = `<li><div class="member">
+                  <h6 class="member-name">${member.name}</h6>
+                  <p class="member-state"><span class="state ${state}"></span>${state}</p>
+                </div></li>`;
+		return memberString;
+	});
+	return members;
 }
